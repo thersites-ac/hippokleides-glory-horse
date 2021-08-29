ac591b93e0219a4c8608fa6bd3d6f7683bd4fb1a
Aaron Cogbill <aaaronc@gmail.com>
2021-08-28 20:25:40 -0400

    extend help message

--- src/main/java/net/picklepark/discord/command/HelpCommand.java
+++ src/main/java/net/picklepark/discord/command/HelpCommand.java
@@ -9,6 +9,8 @@ public class HelpCommand implements DiscordCommand {
     private final GuildMessageReceivedEvent event;
     private static final String instructions = "Commands: ~queue [url], ~skip, ~volume (to get current), ~volume [n] (to set)," +
             " ~louder, ~softer, ~pause, ~unpause, ~gtfo, ~feat [feat name], ~spell [spell name], ~help";
+    private static final String hint = "When you find a feat or spell, click the citation at top (e.g. Core Rulebook, " +
+            "Advanced Player's Guide, etc.) to go to the site.";
 
     public HelpCommand(GuildMessageReceivedEvent event) {
         this.event = event;
@@ -17,5 +19,6 @@ public class HelpCommand implements DiscordCommand {
     @Override
     public void execute() throws IOException {
         event.getChannel().sendMessage(instructions).queue();
+        event.getChannel().sendMessage(hint).queue();
     }
 }
