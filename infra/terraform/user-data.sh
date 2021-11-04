0e6bf26b1056778b41f30f93fcbbf7dece126987
Aaron Cogbill <aaaronc@gmail.com>
2021-11-04 01:22:49 -0400

    compile uber jar and update user data

--- infra/terraform/user-data.sh
+++ infra/terraform/user-data.sh
@@ -1,1 +1,7 @@
-echo "hello world"
+sudo yum install git -y
+sudo amazon-linux-extras install java-openjdk11 -y
+git clone https://github.com/thersites-ac/pathfinder-bot
+cd pathfinder-bot
+./gradlew jar
+# todo: get the token
+java -jar -Dtoken=TOKEN_GOES_HERE build/libs/bot-uber.jar
