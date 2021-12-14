1257bbbf087644f9a7a9df23aad91b8a65d4314d
Aaron Cogbill <aaaronc@gmail.com>
2021-12-13 22:58:25 -0500

    fix compiler error

--- src/main/java/net/picklepark/discord/constants/AudioConstants.java
+++ src/main/java/net/picklepark/discord/constants/AudioConstants.java
@@ -3,8 +3,8 @@ package net.picklepark.discord.constants;
 import java.util.Arrays;
 
 public class AudioConstants {
-    private static final int BYTES_PER_MS = 192;
-    private static final int MS_PER_PACKET = 20;
+    public static final int BYTES_PER_MS = 192;
+    public static final int MS_PER_PACKET = 20;
 
     public static final int PACKET_SIZE = BYTES_PER_MS * MS_PER_PACKET;
     public static final byte[] SILENCE_DATA = new byte[PACKET_SIZE];
