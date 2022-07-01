196e9838586d92346d17c9a5d1034a02e3cb6244
Aaron Cogbill <aaaronc@gmail.com>
2022-07-01 17:26:22 -0400

    add deploy scripts

---
+++ scripts/go.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+TOKEN=`aws secretsmanager get-secret-value --secret-id token --region us-east-2 --query SecretString --output text`
+BITLY_TOKEN=`aws secretsmanager get-secret-value --secret-id bitly_secret --region us-east-2 --query SecretString --output text`
+java -jar -Dtoken=$TOKEN -Dshortener.auth.token=$BITLY_TOKEN build/libs/hippokleides-glory-horse-uber.jar &
+
