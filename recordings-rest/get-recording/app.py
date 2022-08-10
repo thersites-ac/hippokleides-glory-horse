bb6c8818d334f27c95478338ec1210e66b5cbf70
Aaron Cogbill <aaaronc@gmail.com>
2022-08-10 09:15:38 -0400

    dynamodb wrapper lambda

---
+++ recordings-rest/get-recording/app.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+
+
+HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET',
+    'Access-Control-Allow-Headers': '*'
+}
+PREFLIGHT = {
+    'statusCode': 200,
+    'headers': HEADERS
+}
+
+def lambda_handler(event, context):
+    if event['httpMethod'] == 'GET':
+        return get(event)
+    elif event['httpMethod'] == 'OPTIONS':
+        return PREFLIGHT
+
+def get(event):
+    dynamo = boto3.client('dynamodb', region_name = 'us-east-2')
+    key = event['pathParameters']['key']
+    keyd = { 'recording_id': { 'S': key } }
+    print('fetching', keyd)
+    try:
+        resp = dynamo.get_item(TableName = 'hippokleides_recordings', Key = keyd)
+        body = resp['Item']
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({
+                'recording_uri': body['recording_uri']['S'],
+                'waveform_uri': body['waveform_uri']['S']
+            }),
+            'headers': HEADERS
+        }
+    # TODO: actually inspect the exception and return 404 only in case of ResourceNotFound
+    except:
+        return {
+            'statusCode': 404,
+            'body': json.dumps('Not found'),
+            'headers': HEADERS
+        }
