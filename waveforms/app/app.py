76df701188948f37addc7cfe04a6eb62e08d15d4
Aaron Cogbill <aaaronc@gmail.com>
2022-08-12 08:44:32 -0400

    store presigned URL for waveform

--- waveforms/app/app.py
+++ waveforms/app/app.py
@@ -4,6 +4,9 @@ import waveform
 # TODO: fetch the recording ID for the object and write to Dynamo
 def lambda_handler(event, context):
     recordings = boto3.resource('s3').Bucket('discord-recordings')
+    s3_config = boto3.session.Config(region_name = 'us-east-2', signature_version = 's3v4')
+    s3 = boto3.client('s3', config = s3_config)
+    dynamo = boto3.client('dynamodb', region_name = 'us-east-2')
     
     for record in event['Records']:
         key = record['s3']['object']['key']
@@ -16,9 +19,32 @@ def lambda_handler(event, context):
         remote_waveform = remote_waveform_key(key)
         recordings.upload_file(local_waveform, remote_waveform)
         print('uploaded', local_waveform, 'with key', remote_waveform)
+        signature_params = { 'Bucket': 'discord-recordings', 'Key': remote_waveform }
+        signed_waveform_url = s3.generate_presigned_url(
+                'get_object', 
+                Params = signature_params, 
+                ExpiresIn = 300)
+        print('generated signed URL for waveform', signed_waveform_url)
+        tagging_response = s3.get_object_tagging(Key = key, Bucket = 'discord-recordings')
+        tags = transform(tagging_response)
+        recording_id = tags['recording_id'] 
+        print('looked up recording_id', recording_id)
+        keyd = { 'recording_id': { 'S': recording_id} }
+        updates = { 'waveform_uri': { 'Value': { 'S': signed_waveform_url }, 'Action': 'PUT' } }
+        dynamo.update_item(TableName = 'hippokleides_recordings', Key = keyd, AttributeUpdates = updates)
+        print('added signed URL to dynamo record', recording_id)
+
 
 def remote_waveform_key(key):
     parts = key.split('/')
     prefix = parts[0]
     name = parts[1].split('.')[0]
     return prefix + '/waveforms/' + name + '.png'
+
+# ought to be a library function
+def transform(tagging_response):
+    tags = tagging_response.get('TagSet')
+    result = {}
+    for pair in tags:
+        result[pair['Key']] = pair['Value']
+    return result
--- waveforms/app/main.py
+++
@@ -1,4 +0,0 @@
-import sys
-import waveform
-
-waveform.prepare_plot(sys.argv[1], sys.argv[2])
