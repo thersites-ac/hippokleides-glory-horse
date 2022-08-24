23db6dc2df7aad02ecdd73ccc605ca013548fe16
Aaron Cogbill <aaaronc@gmail.com>
2022-08-24 08:15:54 -0400

    one more note

--- src/main/java/cogbog/discord/parse/CommandDsl.java
+++ src/main/java/cogbog/discord/parse/CommandDsl.java
@@ -13,9 +13,7 @@ public class CommandDsl {
     private static final String WHITESPACE = "\\s+";
     private static final String VARIABLE_DECLARATION = "<(\\w+)>";
     // fixme: this is a little too permissive--it'll accept "<@joe" or "bob dole>"--but that's a regex limitation
-    // possible solution: duplicate each variable, then match the copies differently, and pick the successful one in the
-    // Actions class
-    // e.g.: <foo> -> (?<$fooa>(\w|\w.*\w))|(\<\@?<$foob>(\d+)\>)
+    // todo: type variables, so I can apply a different pattern when trying to match a User vs. a generic clip title
     private static final String VARIABLE_SEMANTICS = "(\\<\\@)?(?<$1>(\\\\w|\\\\w.*\\\\w|\\\\d+))\\>?";
 
     private final String dsl;
